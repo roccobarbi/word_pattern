c5c647aa613f82e3b28e9c795b6b3574059fc9a2
rocco barbini <roccobarbi@gmail.com>
2021-08-29 19:27:05 +0200

    generate_pattern_map and helper functions added
    This function is used to precalculate the patterns from a dictionary and to store it in chunks that should make subsequent querying and processing much faster and more efficient.
    
    build_word_pattern can also be used by the main product to calculate a word's pattern and unique characters count so that querying is possible.

--- dictionaries/dictionary.py
+++ dictionaries/dictionary.py
@@ -1,4 +1,7 @@
 # Source: https://github.com/dwyl/english-words
+import os
+import shutil
+import json
 
 
 def get_dictionary(language):
@@ -15,6 +18,61 @@ def list_dictionaries():
     return __dictionary__.keys()
 
 
+def __remove_pattern_dir__(language):
+    patterns_dir = os.path.join(".", "patterns")
+    language_dir = os.path.join(patterns_dir, language)
+    if os.path.exists(patterns_dir) and os.path.isdir(patterns_dir):
+        if os.path.exists(language_dir):
+            if os.path.isdir(language_dir):
+                shutil.rmtree(language_dir)
+            else:
+                os.remove(language_dir)
+
+
+def build_word_pattern(word, language):
+    """Given a word and a language, calculate the word pattern and the number of unique character is has."""
+    pattern = ""
+    unique = 0
+    char_map = {}
+    for char in word:
+        if char not in char_map.keys():
+            char_map[char] = unique
+            unique += 1
+        pattern += __alphabet__[language][char_map[char]]
+    return pattern, unique
+
+
+def __build_pattern_map__(language):
+    patterns = {}
+    wordlist = get_dictionary(language)
+    for word in wordlist:
+        word_pattern, word_unique = build_word_pattern(word, language)
+        if len(word) not in patterns.keys():
+            patterns[len(word)] = {}
+        if word_unique not in patterns[len(word)]:
+            patterns[len(word)][word_unique] = {}
+        if word_pattern not in patterns[len(word)][word_unique]:
+            patterns[len(word)][word_unique][word_pattern] = []
+        patterns[len(word)][word_unique][word_pattern].append(word)
+    return patterns
+
+
+def __build_pattern_map_directories__(language, patterns):
+    language_dir = os.path.join(".", "patterns", language)
+    os.mkdir(language_dir)
+    for word_length in patterns.keys():
+        os.mkdir(os.path.join(language_dir, word_length))
+
+
+def __save_pattern_map__(language, patterns):
+    language_dir = os.path.join(".", "patterns", language)
+    for word_length in patterns.keys():
+        word_length_dir = os.path.join(language_dir, word_length)
+        for word_unique in patterns[word_length].keys:
+            with open(os.path.join(word_length_dir, str(word_unique) + ".json"), "w", encoding="utf-8") as outfile:
+                json.dump(patterns[word_length][word_unique], outfile, ensure_ascii=False, indent=4)
+
+
 def generate_pattern_map(language):
     """Sets up the project with a pattern map for the specified language.
 
@@ -28,7 +86,12 @@ def generate_pattern_map(language):
 
     Patterns should be compiled by assigning "a" to the first letter, "b" to the second, and so on.
     For example, "ancillary" would have the pattern "abcdeeafg"."""
-    pass
+    if language not in __dictionary__.keys():
+        raise ValueError("Unknown language code: " + language)
+    __remove_pattern_dir__(language) # remove any previous pattern map
+    patterns = __build_pattern_map__(language)
+    __build_pattern_map_directories__(language, patterns)
+    __save_pattern_map__(language, patterns)
 
 
 def get_pattern_map(language):
@@ -57,4 +120,8 @@ def lookup_pattern(pattern, language):
 
 __dictionary__ = {
     "en": "english.txt"
+}
+
+__alphabet__ = {
+    "en": "abcdefghijklmnopqrstuvwxyz"
 }
